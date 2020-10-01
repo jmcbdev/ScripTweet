28ac08cedbf462c43a63d0686545a7248f1620e0
kudakadira <kadirakuda@gmail.com>
2020-10-01 22:38:40 +0100

    added autoscraper

--- ScripTweet.py
+++ ScripTweet.py
@@ -7,6 +7,7 @@
 
 import tweepy
 import time
+from autoscraper import AutoScraper
 
 CONSUMER_KEY = 'tyHCivCLoZK7TG9WRzwGdhGiC'
 CONSUMER_SECRET = 'cBvYPDY1ybqEjYP0Hz4tqmayVaUqs45OxbdMDdYJboFFNQShNe'
@@ -18,5 +19,11 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 #create api object
 twitter_API = tweepy.API(auth)
 
+url = 'https://www.biblestudytools.com/topical-verses/the-25-most-read-bible-verses/'
 
-twitter_API.update_status("Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. - Philippians 4:6")
+wanted_list = ["Philippians 4:13:"]
+
+scraper = AutoScraper()
+result = scraper.build(url, wanted_list)
+print(result)
+# twitter_API.update_status("Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. - Philippians 4:6")
